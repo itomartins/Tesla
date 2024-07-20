84637ffaf74a4b9cf80cb345a2582486aae5a956
jailton72 <itomsantos@gmail.com>
2024-07-20 08:39:37 -0300

    Meu segundo projeto HTML CSS JAVA SCRIPT

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let formulario = document.querySelector(".formulario")
+
+console.log(formulario)
+
+function aparecerformulario(){
+    formulario.style.left = "50%"
+}
+
+function desaparecerfomulario(){
+    formulario.style.left = "-50%"
+}
